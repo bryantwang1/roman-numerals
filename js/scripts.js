96d511f2e3e0fc7cea2eb8d4020bc1b4e320c1c9
Bryant Wang and Ryan Back <ryan@>
2016-10-27 09:17:50 -0700

    Add a numberChecker function that returns Not a number! for any input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
+function numberChecker (number) {
+  alert("Not a number!");
+};
+
+function romanConverter(number) {
+
+};
+
+// User Interface below this line
+
 $(function() {
   $("form#roman-numerals").submit(function(event){
     event.preventDefault();
 
     var userInput = $("input#number-input").val();
+    numberChecker(userInput);
     var result = romanConverter(userInput);
     $(".result").text(result);
   });
